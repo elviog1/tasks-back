01c3aad37cbe3f3a28c521e706c1960a6507a3a4
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-12 15:39:35 -0300

    verify

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { createAccessToken } from "../libs/jwt.js";
+import jwt from 'jsonwebtoken'
 
 //CREAR USUARIO
 export const register = async (req, res) => {
@@ -62,6 +63,7 @@ export const logout = (req, res) => {
   return res.sendStatus(200);
 };
 
+//GET PROFILE
 export const profile = async (req, res) => {
   const userFound = await User.findById(req.user.id);
 
@@ -74,3 +76,13 @@ export const profile = async (req, res) => {
     createdAt: userFound.createdAt,
   });
 };
+
+//VERIFY TOKEN
+export const verify = async (req,res)=>{
+  const { token} = req.cookies
+
+  if(!token) return res.status(401).json({message:"Unauthorized"})
+  jwt.verify(token, process.env.TOKEN_SECRET,async(err,user)=>{
+    if(err) return res.status(401).json({message:"Unauthorized"})
+  })
+}
