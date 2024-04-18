9c07337ea78ce1ebf38d69d6045c026f209fabec
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-18 15:07:45 -0300

    fix

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,13 +1,20 @@
-import jwt from 'jsonwebtoken'
-export const authRequired = (req,res,next) =>{
-    const {token} = req.cookies
+import jwt from "jsonwebtoken";
+export const authRequired = (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "No token, authorization denied" });
 
-    if(!token) return res.status(401).json({message: "No token, authorization denied"})
-
-    jwt.verify(token, process.env.TOKEN_SECRET, (err,user) =>{
-        if(err) return res.status(403).json({message: "Invalid token"})
-        req.user = user //guardo en REQ la prop user, el valor de USER
-    })
-
-    next()
-}
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+      req.user = user; //guardo en REQ la prop user, el valor de USER
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
