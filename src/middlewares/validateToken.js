e7b79b90e37d9b37d083340702fa9f526b690a0c
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-18 14:27:24 -0300

    fix

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken'
-export const authRequired =  async(req,res,next) =>{
+export const authRequired = (req,res,next) =>{
     const {token} = req.cookies
 
-    if(!token) return await res.status(401).json({message: "No token, authorization denied"})
+    if(!token) return res.status(401).json({message: "No token, authorization denied"})
 
-     jwt.verify(token, process.env.TOKEN_SECRET, (err,user) =>{
+    jwt.verify(token, process.env.TOKEN_SECRET, (err,user) =>{
         if(err) return res.status(403).json({message: "Invalid token"})
         req.user = user //guardo en REQ la prop user, el valor de USER
     })
