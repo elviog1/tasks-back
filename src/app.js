342409bdec4a3c6a0d3e41e1ab72e93cf9d727f0
Elvio <elvio.galeano@gmail.com>
2024-04-19 15:31:57 -0300

    cookies

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-import taskRoutes from "./routes/tasks.routes.js"
+import taskRoutes from "./routes/tasks.routes.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-app.use(cors({
-    origin: process.env.FRONT_URL,
-    credentials:true
-}))
-app.use(cookieParser())
-app.use(express.json())
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
 
-app.use("/api",authRoutes);
-app.use("/api",taskRoutes);
+app.use((req, res, next) => {
+  const { token } = req.cookies;
+  res.cookie("token", token, {
+    sameSite: "none",
+    secure: true,
+  });
+  console.log(token);
+  next();
+});
+
+app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
 
 export default app;
 
